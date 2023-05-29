cc57a4a0e20fcc93f59c9ecc61c58f2fe2c2550f
Michal Morawiec <yMichaelmorawiec@wp.pl>
2023-05-29 19:30:18 +0200

    Cmake init

---
+++ build/CMakeFiles/object.o.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/object.o.dir/object.cpp.o"
+  "CMakeFiles/object.o.dir/object.cpp.o.d"
+  "object.o"
+  "object.o.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/object.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
